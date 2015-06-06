8b2dae446ae18f9e3b44348df7d0836bd29711d9
a3626a <a3626a@naver.com>
2015-06-06 22:17:07 +0900

    solve merge error

--- src/Dict.java
+++ src/Dict.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Dict {
-	
+
 	private ArrayList<Tuple> tuples;
 
 	public Dict() {
@@ -11,53 +11,135 @@ public class Dict {
 	public void insert(int docID, int location) {
 		tuples.add(new Tuple(docID, location));
 	}
-	
+
 	public void compress() {
 		int sizeDict = tuples.size();
-		for (int i=sizeDict-1; i>=1; i--) {
-			int diff = tuples.get(i).getDocID() - tuples.get(i-1).getDocID();
+		for (int i = sizeDict - 1; i >= 1; i--) {
+			int diff = tuples.get(i).getDocID() - tuples.get(i - 1).getDocID();
 			tuples.get(i).setDocID(diff);
 		}
 	}
-	
+
 	public Dict mergeCompressedDict(Dict dictionary) {
 		// Exception Handling - one of dictionaries is empty
 		if (this.tuples.isEmpty() || dictionary.tuples.isEmpty()) {
 			return new Dict();
 			// return empty dictionary
 		}
-		
+
 		Dict mergedDict = new Dict();
-		
+
 		int doc1;
 		// pointer variable to store document id of d1
 		int doc2;
 		// pointer variable to store document id of d2
-		int docM=0;
+		int docM = 0;
 		// pointer variable to store document id of merged dictionary
-		
+
 		doc1 = this.tuples.get(0).getDocID();
 		doc2 = dictionary.tuples.get(0).getDocID();
 		// initialization
-		
+
 		int p1 = 1;
 		int p2 = 1;
 		// pointer variable to track index of d1, d2 (next to read)
-		// these are initialized to 1 because we already read the first entry to initialize doc1 and doc2
-		
+		// these are initialized to 1 because we already read the first entry to
+		// initialize doc1 and doc2
+
 		while (p1 < this.tuples.size() && p2 < dictionary.tuples.size()) {
 			if (doc1 == doc2) {
 				// add tuples that exist on d1 and d2
-				mergedDict.tuples.add(new Tuple(doc1-docM, this.tuples.get(p1-1).getLocation()));
-				docM=doc1;
-				while (p1 < this.tuples.size() && this.tuples.get(p1).getDocID()==0) {
-					mergedDict.tuples.add(new Tuple(0,this.tuples.get(p1).getLocation()));
+				mergedDict.tuples.add(new Tuple(doc1 - docM, this.tuples.get(
+						p1 - 1).getLocation()));
+				docM = doc1;
+				while (p1 < this.tuples.size()
+						&& this.tuples.get(p1).getDocID() == 0) {
+					mergedDict.tuples.add(new Tuple(0, this.tuples.get(p1)
+							.getLocation()));
+					p1++;
+				}
+
+				mergedDict.tuples.add(new Tuple(0, dictionary.tuples
+						.get(p2 - 1).getLocation()));
+				while (p2 < dictionary.tuples.size()
+						&& dictionary.tuples.get(p2).getDocID() == 0) {
+					mergedDict.tuples.add(new Tuple(0, dictionary.tuples
+							.get(p2).getLocation()));
+					p2++;
+				}
+				
+				if (p1 < this.tuples.size()) {
+					doc1 += this.tuples.get(p1).getDocID();
 					p1++;
 				}
 				
-				mergedDict.tuples.add(new Tuple(0, dictionary.tuples.get(p2-1).getLocation()));
-				while (p2 < dictionary.tuples.size() && dictionary.tuples.get(p2).getDocID()==0) {
-					mergedDict.tuples.add(new Tuple(0,dictionary.tuples.get(p2).getLocation()));
+				if (p2 < this.tuples.size()) {
+					doc2 += this.tuples.get(p2).getDocID();
+					p2++;
+				}
+				
+			} else if (doc1 > doc2) {
+				// traverse tuples
+				doc2 += dictionary.tuples.get(p2).getDocID();
+				p2++;
+			} else if (doc1 < doc2) {
+				// we can use just 'else' but for easy-to-read code
+				// traverse tuples
+				doc1 += this.tuples.get(p1).getDocID();
+				p1++;
+			}
+		}
+
+		return mergedDict;
+	}
+
+	public Dict excludeCompressedDict(Dict dictionary) {
+		// Exception Handling - one of dictionaries is empty
+		if (this.tuples.isEmpty() || dictionary.tuples.isEmpty()) {
+			return this;
+			// this code looks weird but right :)
+			// if this.tuples.isEmpty()==true, return empty dict(this)
+			// if dictionary.tuples.isEmpty()==true, return same dict(this)
+		}
+
+		Dict excludeDict = new Dict();
+
+		int doc1;
+		// pointer variable to store document id of d1
+		int doc2;
+		// pointer variable to store document id of d2
+		int docM = 0;
+		// pointer variable to store document id of merged dictionary
+
+		doc1 = this.tuples.get(0).getDocID();
+		doc2 = dictionary.tuples.get(0).getDocID();
+		// initialization
+
+		int p1 = 1;
+		int p2 = 1;
+		// pointer variable to track index of d1, d2 (next to read)
+		// these are initialized to 1 because we already read the first entry to
+		// initialize doc1 and doc2
+
+		while (p1 < this.tuples.size() && p2 < dictionary.tuples.size()) {
+			if (doc1 == doc2) {
+				// add tuples that exist on d1 and d2
+				excludeDict.tuples.add(new Tuple(doc1 - docM, this.tuples.get(
+						p1 - 1).getLocation()));
+				docM = doc1;
+				while (p1 < this.tuples.size()
+						&& this.tuples.get(p1).getDocID() == 0) {
+					excludeDict.tuples.add(new Tuple(0, this.tuples.get(p1)
+							.getLocation()));
+					p1++;
+				}
+
+				excludeDict.tuples.add(new Tuple(0, dictionary.tuples
+						.get(p2 - 1).getLocation()));
+				while (p2 < dictionary.tuples.size()
+						&& dictionary.tuples.get(p2).getDocID() == 0) {
+					excludeDict.tuples.add(new Tuple(0, dictionary.tuples
+							.get(p2).getLocation()));
 					p2++;
 				}
 			} else if (doc1 > doc2) {
@@ -71,20 +153,17 @@ public class Dict {
 				p1++;
 			}
 		}
-		
-		return mergedDict;
+
+		return excludeDict;
 	}
-	
-	public Dict excludeCompressedDict (Dict dictionary) {
-		return new Dict();
-	}
-	
+
 	public void printDict() {
 		System.out.println("Search results:");
 		for (Tuple t : this.tuples) {
-			t.printTuple(); System.out.print(" ");
+			t.printTuple();
+			System.out.print(" ");
 		}
 		System.out.println();
 	}
-	
+
 }
