d133a86316158444e5535d2b81b416cdd95b2f74
JaewonD <wjdwodnjs147@naver.com>
2015-06-06 22:59:09 +0900

    Comments on Node.java

--- src/Node.java
+++ src/Node.java
@@ -10,11 +10,28 @@ public class Node {
 		value = character;
 		nextNodes = new ArrayList<Node>();
 	}
-
+	
+	/*
+	 *  method: createDictionary
+	 *  inputs: none
+	 *  outputs: none
+	 *  
+	 *  Initializing Dict object is separated because this Node object is designed to have
+	 *  initialized Dict object when at least one of that key is found. Otherwise it remains
+	 *  as null.
+	 */
 	public void createDictionary() {
 		dictionary = new Dict();
 	}
-
+	
+	/*
+	 *  method: find
+	 *  inputs: String key
+	 *  outputs: A node corresponds to the last char of key.
+	 *  
+	 *  This method works recursively. It finds a node corresponds to the last character of key.
+	 *  If such node does not exist it returns null.
+	 */
 	public Node find(String key) {
 		if (key.length() > 1) {
 			char first = key.charAt(0);
@@ -34,6 +51,16 @@ public class Node {
 		}
 	}
 
+	/*
+	 *  method: make
+	 *  inputs: String key
+	 *  outputs: A node corresponds to the last char of key.
+	 *  
+	 *  This method works recursively. It finds a node corresponds to the last character of key.
+	 *  The main difference with find method is, if such node does not exist this method extends
+	 *  the tree to contain the key and then return the node of the last character of key, while
+	 *  find method returns null.
+	 */
 	public Node make(String key) {
 		if (key.length() > 1) {
 			char first = key.charAt(0);
@@ -65,10 +92,26 @@ public class Node {
 		}
 	}
 	
+	/*
+	 *  method: addData
+	 *  inputs: Two integers docID and location.
+	 *  outputs: none
+	 *  
+	 *  Since the corresponding node is found by make/find methods, this method just simply
+	 *  adds the tuple (docID, location) to this Node object's Dict object.
+	 */
 	public void addData(int docID, int location) {
 		this.dictionary.insert(docID, location);
 	}
 	
+	/*
+	 *  method: traverseAndCompress
+	 *  inputs: none
+	 *  outputs: none
+	 *  
+	 *  This method works recursively. It traverses through the tree, and if a node has
+	 *  initialized Dict object, this method delta-compresses that object.
+	 */
 	public void traverseAndCompress () {
 		if (this.dictionary != null) {
 			this.dictionary.compress();
@@ -78,10 +121,20 @@ public class Node {
 		}
 	}
 
+	/*
+	 *  method: getValue
+	 *  inputs: none
+	 *  outputs: char value of this node.
+	 */
 	public char getValue() {
 		return this.value;
 	}
 	
+	/*
+	 *  method: getList
+	 *  inputs: none
+	 *  outputs: A Dict object of this Node object.
+	 */
 	public Dict getList() {
 		return this.dictionary;
 	}
